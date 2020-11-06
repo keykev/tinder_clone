db351f3dab7fa62aaa300df44aff87b97b58457b
Kevin Yang <phyang018@gmail.com>
2020-11-05 19:55:14 -0800

    sdf

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use('/',require('./routes/tinder'))
 //Serve static assets in production
 if(process.env.NODE_ENV === 'production') {
   //set static folder
-  app.use(express.static('client/build'))
+  app.use('*', express.static(path.join(__dirname, "client", "build")))
 
   //sends index file.
   app.get("*",(req,res) => {
